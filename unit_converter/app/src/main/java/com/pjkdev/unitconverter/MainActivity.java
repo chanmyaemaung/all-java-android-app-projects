d13078c34b7bf58155c6f7e6cc2e0607999872ea
Chen Lay <maungmaungyoker.mmy@gmail.com>
2023-11-05 12:42:22 +0630

    :bug: Fixed bugs and added a few conditions

--- unit_converter/app/src/main/java/com/pjkdev/unitconverter/MainActivity.java
+++ unit_converter/app/src/main/java/com/pjkdev/unitconverter/MainActivity.java
@@ -1,12 +1,13 @@
 package com.pjkdev.unitconverter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -18,22 +19,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Select all corresponding variables
+        welcomeText = findViewById(R.id.welcomeText);
+        editText = findViewById(R.id.editText);
+        showConvertText = findViewById(R.id.showConvertText);
+        convertBtn = findViewById(R.id.convertBtn);
+
+        // Welcome text
+        welcomeText.setText("Welcome to my kilo to pound converter app.");
+
+        // Default EditText value
+        editText.setText("1");
+
         convertBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Welcome text
-                welcomeText.setText("Welcome to kilo to pound converter app.");
-
                 // Getting user input value
                 String inputText = editText.getText().toString();
 
                 // Convert to double
                 Double kilos = Double.parseDouble(inputText);
 
+                // Convert to pounds
                 double pounds = convertToPound(kilos);
 
                 // Show the result in pounds
-                showConvertText.setText("" + pounds);
+                showConvertText.setText("" + pounds + "lb");
+
+                // Clear the input
+                editText.setText("1");
+
+                // Show toast message with result
+                Toast.makeText(MainActivity.this, "Converted to pounds: " + pounds + "lb", Toast.LENGTH_LONG).show();
             }
         });
     }
